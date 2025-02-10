18c66e05dedcfb6cb9e406469d31536caff94899
Tiobilito <andre.marin1135@alumnos.udg.mx>
2025-02-10 16:30:04 -0600

    Agregar archivos de proyecto.

---
+++ Practica 4/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Practica_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer4 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.splitContainer5 = new System.Windows.Forms.SplitContainer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer4)).BeginInit();
+            this.splitContainer4.Panel1.SuspendLayout();
+            this.splitContainer4.Panel2.SuspendLayout();
+            this.splitContainer4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer5)).BeginInit();
+            this.splitContainer5.Panel1.SuspendLayout();
+            this.splitContainer5.Panel2.SuspendLayout();
+            this.splitContainer5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            this.toolStripContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.toolStripContainer1.BottomToolStripPanelVisible = false;
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.splitContainer1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(800, 450);
+            this.toolStripContainer1.LeftToolStripPanelVisible = false;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.RightToolStripPanelVisible = false;
+            this.toolStripContainer1.Size = new System.Drawing.Size(800, 450);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            this.toolStripContainer1.TopToolStripPanelVisible = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer3);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 223;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.richTextBox1);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.button1);
+            this.splitContainer3.Size = new System.Drawing.Size(800, 223);
+            this.splitContainer3.SplitterDistance = 548;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(548, 223);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 223);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.splitContainer4);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer5);
+            this.splitContainer2.Size = new System.Drawing.Size(800, 223);
+            this.splitContainer2.SplitterDistance = 396;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // splitContainer4
+            // 
+            this.splitContainer4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer4.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer4.Name = "splitContainer4";
+            this.splitContainer4.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer4.Panel1
+            // 
+            this.splitContainer4.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer4.Panel2
+            // 
+            this.splitContainer4.Panel2.Controls.Add(this.label3);
+            this.splitContainer4.Size = new System.Drawing.Size(396, 223);
+            this.splitContainer4.SplitterDistance = 42;
+            this.splitContainer4.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Recorrido:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 29);
+            this.label3.TabIndex = 1;
+            // 
+            // splitContainer5
+            // 
+            this.splitContainer5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer5.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer5.Name = "splitContainer5";
+            this.splitContainer5.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer5.Panel1
+            // 
+            this.splitContainer5.Panel1.Controls.Add(this.label2);
+            // 
+            // splitContainer5.Panel2
+            // 
+            this.splitContainer5.Panel2.Controls.Add(this.label4);
+            this.splitContainer5.Size = new System.Drawing.Size(400, 223);
+            this.splitContainer5.SplitterDistance = 44;
+            this.splitContainer5.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Resultado:";
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(400, 175);
+            this.label4.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer4.Panel1.ResumeLayout(false);
+            this.splitContainer4.Panel1.PerformLayout();
+            this.splitContainer4.Panel2.ResumeLayout(false);
+            this.splitContainer4.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer4)).EndInit();
+            this.splitContainer4.ResumeLayout(false);
+            this.splitContainer5.Panel1.ResumeLayout(false);
+            this.splitContainer5.Panel1.PerformLayout();
+            this.splitContainer5.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer5)).EndInit();
+            this.splitContainer5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.SplitContainer splitContainer4;
+        private System.Windows.Forms.SplitContainer splitContainer5;
+    }
+}
+
---
+++ Practica 4/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Practica_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Obtener la expresión ingresada en el RichTextBox
+            string expresion = richTextBox1.Text;
+
+            try
+            {
+                // Crear el analizador sintáctico y evaluar la expresión
+                Nodo arbol = AnalizadorSintactico.Parse(expresion);
+                double resultado = arbol.Evaluar();
+
+                // Mostrar el resultado en el Label
+                label4.Text = $"Resultado: {resultado}";
+
+                // Mostrar el recorrido del árbol en el otro Label
+                label3.Text = ObtenerRecorrido(arbol);
+            }
+            catch (Exception ex)
+            {
+                // Manejar errores (por ejemplo, división entre cero)
+                label4.Text = $"\nError: {ex.Message}";
+                label3.Text = string.Empty;
+            }
+        }
+
+        // Método para obtener el recorrido del árbol en preorden
+        private string ObtenerRecorrido(Nodo nodo)
+        {
+            if (nodo is Hoja hoja)
+            {
+                return $"{hoja.Tipo}({hoja.Valor})";
+            }
+            else if (nodo is OperacionBinaria operacion)
+            {
+                string izquierda = ObtenerRecorrido(operacion.Izquierda);
+                string derecha = ObtenerRecorrido(operacion.Derecha);
+                return $"{operacion.Operador}({izquierda}, {derecha})";
+            }
+            else
+            {
+                return "Nodo desconocido";
+            }
+        }
+    }
+
+    // Clases de nodos y analizador sintáctico
+    public abstract class Nodo
+    {
+        public abstract double Evaluar();
+    }
+
+    public class Hoja : Nodo
+    {
+        public string Tipo { get; }
+        public double Valor { get; }
+
+        public Hoja(string tipo, double valor)
+        {
+            Tipo = tipo;
+            Valor = valor;
+        }
+
+        public override double Evaluar()
+        {
+            return Valor;
+        }
+    }
+
+    public class OperacionBinaria : Nodo
+    {
+        public Nodo Izquierda { get; }
+        public Nodo Derecha { get; }
+        public char Operador { get; }
+
+        public OperacionBinaria(Nodo izquierda, Nodo derecha, char operador)
+        {
+            Izquierda = izquierda;
+            Derecha = derecha;
+            Operador = operador;
+        }
+
+        public override double Evaluar()
+        {
+            double izq = Izquierda.Evaluar();
+            double der = Derecha.Evaluar();
+
+            switch (Operador)
+            {
+                case '+':
+                    return izq + der;
+                case '-':
+                    return izq - der;
+                case '*':
+                    return izq * der;
+                case '/':
+                    if (der != 0)
+                        return izq / der;
+                    else
+                        throw new DivideByZeroException("Error: División entre cero.");
+                default:
+                    throw new ArgumentException($"Operador no válido: {Operador}");
+            }
+        }
+    }
+
+    public static class AnalizadorSintactico
+    {
+        private static List<string> tokens;
+        private static int index;
+
+        public static Nodo Parse(string expresion)
+        {
+            // Tokeniza la expresión
+            tokens = Tokenizar(expresion);
+            index = 0;
+
+            // Construye el árbol de análisis sintáctico
+            return Expresion();
+        }
+
+        private static List<string> Tokenizar(string expresion)
+        {
+            List<string> tokens = new List<string>();
+            int i = 0;
+
+            while (i < expresion.Length)
+            {
+                char c = expresion[i];
+
+                // Ignorar espacios en blanco
+                if (char.IsWhiteSpace(c))
+                {
+                    i++;
+                    continue;
+                }
+
+                // Operadores
+                if (c == '+' || c == '-' || c == '*' || c == '/')
+                {
+                    tokens.Add(c.ToString());
+                    i++;
+                }
+                // Paréntesis
+                else if (c == '(' || c == ')')
+                {
+                    tokens.Add(c.ToString());
+                    i++;
+                }
+                // Números
+                else if (char.IsDigit(c))
+                {
+                    int j = i;
+                    while (j < expresion.Length && char.IsDigit(expresion[j]))
+                        j++;
+
+                    string numero = expresion.Substring(i, j - i);
+                    tokens.Add(numero);
+                    i = j;
+                }
+                else
+                {
+                    // Error: Token no válido
+                    throw new ArgumentException($"Error: Token no válido en posición {i}");
+                }
+            }
+
+            return tokens;
+        }
+
+        private static Nodo Expresion()
+        {
+            Nodo izquierda = Termino();
+
+            while (index < tokens.Count && (tokens[index] == "+" || tokens[index] == "-" || tokens[index] == "*" || tokens[index] == "/"))
+            {
+                char operador = tokens[index][0];
+                index++;
+
+                Nodo derecha = Termino();
+                izquierda = new OperacionBinaria(izquierda, derecha, operador);
+            }
+
+            return izquierda;
+        }
+
+        private static Nodo Termino()
+        {
+            if (tokens[index] == "(")
+            {
+                index++;
+                Nodo expresion = Expresion();
+                if (tokens[index] != ")")
+                    throw new ArgumentException("Error: Paréntesis desbalanceados");
+                index++;
+                return expresion;
+            }
+            else if (EsNumero(tokens[index]))
+            {
+                double valor = double.Parse(tokens[index]);
+                index++;
+                return new Hoja("num", valor);
+            }
+            else
+            {
+                throw new ArgumentException($"Error: Token no válido: {tokens[index]}");
+            }
+        }
+
+        private static bool EsNumero(string token)
+        {
+            // Intenta convertir el token a un número
+            return double.TryParse(token, out _);
+        }
+    }
+}
